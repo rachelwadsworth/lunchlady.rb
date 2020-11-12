88fa79285718715f1903abfdd765eb24219c3456
rachelwadsworth-smith <rachel.wadsworthsmith@gmail.com>
2020-11-12 14:38:07 -0700

    1 add git help menu

--- lunchlady.rb
+++ lunchlady.rb
@@ -21,3 +21,27 @@ def menu
 end
 
 menu
+
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+  menu
+end
+
+def menu
+  puts '1: Enter git command'
+  puts '2: Exit'
+  choice = gets.to_i
+  case choice
+  when 1
+    puts 'Enter git command'
+    puts_git(gets.strip)
+    menu
+  when 2
+    exit
+  else
+    puts 'Invalid choice'
+    menu
+  end
+end
+
+menu
