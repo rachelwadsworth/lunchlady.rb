3d9411bb95860dfef76646a73ab0b487b3ebccb6
rachelwadsworth-smith <rachel.wadsworthsmith@gmail.com>
2020-11-12 22:47:38 -0700

    Emypty commit

--- lunchlady.rb
+++ lunchlady.rb
@@ -1,47 +1,83 @@
-def puts_git(cmd)
-  puts `git #{cmd} -h`
-  menu
-end
+#make a list of entrees
+
+@entrees= [
+  
+  {name: "spaghetti",price:"5.00"},
+  {name:"Hot Dog",price:"100.00"},
+  {name:"Pizza",price:"20.00",},
+]
+#make a list of appatizers
+  @appatizers=[
+  {name: "soup",price:"8.00"},
+  {name:"salad",price:"6.00"},
+  {name:"Fries",price:"10.00"},
+  ]
+
+  #user input choices ifs
+@order = []
 
 def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
-  choice = gets.to_i
-  case choice
-  when 1
-    puts 'Enter git command'
-    puts_git(gets.strip)
+    puts 'What would your appatizer this evening?'
+    puts '1) Spaghetti (5.00)'
+    puts '2) hot dog (100'
+    puts '3) pizza (20.00)'
+    puts '4) Start Over'
+    @user_main_choice = gets.chomp 
+    case @user_Entrees
+    when '1' 
+      entrees = @main_menu[0]
+    when '2'
+      entrees = @main_menu[1]
+    when '3'
+      entrees = @main_menu[2]
+    when '4'
+      @order.clear 
+      menu
+    else
+      puts 'Ivalid entry, please try again.'
+      menu 
+    end
+    @order << Entrees
+  end 
+
+
+  def appatizers
+  puts "what appatizers would you like?"
+  puts '1) soup (2.00)'
+  puts '2) salad (6.00)'
+  puts '3) fries(10.00)'
+  puts '4) start over'
+  @user_appatizers =gets.chomp
+  case @user_appatizers
+  when '1' 
+    user_appatizers = @appatizers[0]
+  when '2'
+    user_appatizers = @appatizers[1]
+  when '3'
+    user_appatizers = @appatizers[2]
+  when '4'
+    @order.clear 
     menu
-  when 2
-    exit
   else
-    puts 'Invalid choice'
-    menu
+    puts 'Ivalid entry, please try again.'
+    menu 
   end
+  @order << appatizers
+end 
+
+def total(entrees, appatizers1, appatizers2)
+  puts "Here is your order #{entree[:name]}, #{appatizers1[:name]} and #{appatizers2[:name]}."
+  cost = entrees[:price] + appatizers1[:price] + appatizers2[price]
+  puts "your total is #{cost}."
 end
 
 menu
 
-def puts_git(cmd)
-  puts `git #{cmd} -h`
-  menu
+appatizers_count =0
+
+while appatizer_count < 2 do
+  appatizers_count = appatizers_count + 1
+  appatizers
 end
 
-def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
-  choice = gets.to_i
-  case choice
-  when 1
-    puts 'Enter git command'
-    puts_git(gets.strip)
-    menu
-  when 2
-    exit
-  else
-    puts 'Invalid choice'
-    menu
-  end
-end
-
-menu
+total(@order[0],@order[1],@order[2])
